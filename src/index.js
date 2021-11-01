c63d19971b5c882e7e217d9a255ba0c2cb264a77
Katia610 <93476065+Katia610@users.noreply.github.com>
2021-11-01 15:06:44 +0000

    Added weather app project to github
    Weather app project added on github (SheCodes Plus - week 6 - lesson 4 homewokr)

---
+++ src/index.js
@@ -0,0 +1,127 @@
+function showCurrentWeatherConditions(response) {
+  let currentTemperature = Math.round(response.data.main.temp);
+  let temperature = document.querySelector("#current-temperature");
+  temperature.innerHTML = `${currentTemperature}`;
+
+  let cityDisplay = document.querySelector("#display-city");
+  let currentCity = response.data.name;
+  cityDisplay.innerHTML = currentCity;
+
+  let currentWeatherDescription = response.data.weather[0].main;
+  let weatherDescription = document.querySelector("#display-comment");
+  weatherDescription.innerHTML = `${currentWeatherDescription}`;
+
+  let highTemperature = Math.round(response.data.main.temp_max);
+  let lowTemperature = Math.round(response.data.main.temp_min);
+  let highLowDisplay = document.querySelector("#high-low");
+  highLowDisplay.innerHTML = `High / Low: ${highTemperature}°C / ${lowTemperature}°C`;
+
+  let currentRealFeel = Math.round(response.data.main.feels_like);
+  let realFeel = document.querySelector("#real-feel");
+  realFeel.innerHTML = `Real Feel: ${currentRealFeel}°C`;
+
+  let currentHumidity = response.data.main.humidity;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `Humidity: ${currentHumidity}%`;
+}
+
+function searchCity(city) {
+  let apiKey = "8f96c22e12424a3fb4624c20be074503";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showCurrentWeatherConditions);
+}
+
+function findCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-search");
+  let city = cityInput.value;
+  searchCity(city);
+}
+
+function showPosition(position) {
+  let apiKey = "8f96c22e12424a3fb4624c20be074503";
+  let units = "metric";
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showCurrentWeatherConditions);
+}
+
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function displayTemperatureInCelsius(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#current-temperature");
+  temperature.innerHTML = 14;
+}
+
+function displayTemperatureInFahrenheit(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#current-temperature");
+  temperature.innerHTML = 57;
+}
+
+let now = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let date = now.getDate();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let year = now.getFullYear();
+
+let currentDate = document.querySelector("#current-date");
+currentDate.innerHTML = `${day} ${date} ${month} ${year}`;
+
+let hour = now.getHours();
+let minute = now.getMinutes();
+
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+
+if (minute < 10) {
+  minute = `0${minute}`;
+}
+
+let currentTime = document.querySelector("#current-time");
+currentTime.innerHTML = `${hour}:${minute}`;
+
+let form = document.querySelector("form");
+form.addEventListener("submit", findCity);
+
+let button = document.querySelector("#current-location");
+button.addEventListener("click", getCurrentPosition);
+
+let unitCelsius = document.querySelector("#celsius");
+unitCelsius.addEventListener("click", displayTemperatureInCelsius);
+
+let unitFahrenheit = document.querySelector("#fahrenheit");
+unitFahrenheit.addEventListener("click", displayTemperatureInFahrenheit);
+
+searchCity("London");
